08c795c4cd0086e5eac1b853092c6f22bc5335a6
Chicibelito70 <Carlosvillavizar07@gmail.com>
2023-08-14 22:06:31 -0400

    Feat: fin del proyecto.

---
+++ README.md
@@ -0,0 +1,8 @@
+<p> 
+Un cronómetro es como un reloj especial que nos ayuda a contar el tiempo en juegos y actividades divertidas. Puedes pensar en él como un contador mágico que nos dice cuánto tiempo ha pasado desde que comenzamos algo hasta que terminamos. Por ejemplo, si estamos corriendo en el parque, podemos presionar un botón en el cronómetro cuando comenzamos a correr, y otro botón cuando terminamos. Entonces, el cronómetro nos dirá exactamente cuánto tiempo hemos corrido.
+</p>
+
+<img>
+
+![Alt text](image.png)
+</img>
--- logic.js
+++ logic.js
@@ -1,25 +1,35 @@
-/* Declaracion de variables */
 let tiempoRef = Date.now()
 let cronometrar = false
-let acumulado = 0 
-/* logia implentada */
+let acumulado = 0
+
+function iniciar() {
+    cronometrar = true
+}
+function pausar() {
+    cronometrar = false
+}
+function reiniciar() {
+    acumulado = 0
+}
+
 setInterval(() => {
     let tiempo = document.getElementById("tiempo")
-    if (cronometrar){
+    if (cronometrar) {
         acumulado += Date.now() - tiempoRef
     }
     tiempoRef = Date.now()
-    tiempo.innerHTML = acumulado
-}, 1000/60);
+    tiempo.innerHTML = formatearMS(acumulado)
+}, 1000 / 60);
 
-/* Funcion que realiza el formateo de el cronometro */
-function formatearMS(tiempo_ms){
+function formatearMS(tiempo_ms) {
     let MS = tiempo_ms % 1000
-    let S = Math.floor(((tiempo_ms - MS )/1000)% 60)
-    let M = Math.floor(((S / 60 ))% 60)
-    let H = Math.floor((M / 60))
-    Number.prototype.ceros = function (n){
-        return (this + "").padStart(n,0)
+    let St = Math.floor(((tiempo_ms - MS) / 1000))
+    let S = St%60
+    let M = Math.floor((St / 60) % 60)
+    let H = Math.floor((St/60 / 60))
+    Number.prototype.ceros = function (n) {
+        return (this + "").padStart(n, 0)
     }
-    return H.ceros(2) + ":" +  M.ceros(2) + ":" + S.ceros(2) + "." + MS.ceros(3)
+    return H.ceros(2) + ":" + M.ceros(2) + ":" + S.ceros(2)
+        + "." + MS.ceros(3)
 }
